2c9b449795f29533d9b922dbfc31b9f3cf521bb2
abdul-basit-khan-dev <basitkhanbasit640@gmail.com>
2024-03-27 16:39:56 +0500

    Added new CORS

--- functions/app.js
+++ functions/app.js
@@ -96,6 +96,14 @@ const app = express();
 
 app.use(cors()); // Enable CORS
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
